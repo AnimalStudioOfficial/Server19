8a8bc906194bcf521f193e692d0d73b27f050d60
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2022-12-14 20:33:29 -0500

    Update data.js

--- src/routes/data.js
+++ src/routes/data.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios')
 const router = express.Router();
 
 const asyncMiddleware = fn => (req, res, next) => {
@@ -6,8 +7,24 @@ const asyncMiddleware = fn => (req, res, next) => {
     .catch(next);
 };
 
-router.get('/', asyncMiddleware(async (req, res) => {
+router.get('/assets/:id', asyncMiddleware(async (req, res) => {
  //res.json(ajax.responseText);
+    let id = req.params?.id
+    if(!id) {
+        return res.status(403).json({ error: "Did not provide asset id", })
+    }
+
+    try {
+        id = encodeURIComponent(id)
+        const response = await axios.get(`https://assetdelivery.roblox.com/v1/asset?id=${id}`)
+        return res
+            .status(response.status)
+            .header(response.headers)
+            .send(response.data)
+    } catch (ex) {
+        return res.status(500).json({ error: 'Error occured while making asset request', message: ex.message })
+    }
+})
 }));
 
 
