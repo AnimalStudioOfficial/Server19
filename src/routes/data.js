c685a8b62810d4f89e4d4c8b07646e168090d3f8
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2022-12-14 20:02:00 -0500

    Update data.js

--- src/routes/data.js
+++ src/routes/data.js
@@ -7,16 +7,7 @@ const asyncMiddleware = fn => (req, res, next) => {
 };
 
 router.get('/', asyncMiddleware(async (req, res) => {
-  //const todos = await prisma.TodoItem.findMany();
-  var ajax = new XMLHttpRequest();
-ajax.onreadystatechange = function() {
-if (ajax.readyState == 4) {
- alert(ajax.responseText);
- res.json(ajax.responseText);
-}
-};
-ajax.open("GET", "ajax.php", true);
-ajax.send(null);
+ //res.json(ajax.responseText);
 }));
 
 
